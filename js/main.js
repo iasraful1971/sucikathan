3fbcd4b6d575ffa18752c078277b1b2c56ac42fa
asraful <iasraful107@gmail.com>
2021-07-05 20:54:58 +0600

    heeloo

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.mask-slider').slick({
+   slidesToShow:3,
+   slidesToScroll:1,
+   prevArrow:".prev-button",
+   nextArrow:".next-button",
+   autoplay:true
+
+
+
+    });
+});
